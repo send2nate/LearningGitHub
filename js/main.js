3e4ff5e77ac6c023cea15f6b45c0bc5fb4e8a6c3
Nate Oakley <nateoakley@gmail.com>
2018-06-28 23:09:28 -0700

    more safari support pt. 2
    unique variable names

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 var color = '#' + ((1<<24)*Math.random()|0).toString(16).padStart(6,0);
-var body = document.querySelector("body");
-body.style.background = color;
+var bodyOne = document.querySelector("body");
+bodyOne.style.background = color;
