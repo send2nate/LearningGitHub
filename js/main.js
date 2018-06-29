cd82425c69a9e6d075ea076d1a5e32368618f50a
Nate Oakley <nateoakley@gmail.com>
2018-06-28 23:07:54 -0700

    more safari support
    changed "let" to "var"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-let color = '#' + ((1<<24)*Math.random()|0).toString(16).padStart(6,0);
-let body = document.querySelector("body");
+var color = '#' + ((1<<24)*Math.random()|0).toString(16).padStart(6,0);
+var body = document.querySelector("body");
 body.style.background = color;
