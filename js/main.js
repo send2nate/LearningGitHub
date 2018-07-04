caf543367a24ff4e34e4a41916274df85952f24a
Nate Oakley <nateoakley@gmail.com>
2018-07-04 08:41:03 -0700

    changed random background code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-var color = '#' + ((1<<24)*Math.random()|0).toString(16).padStart(6,0);
+var color = randomBackground();
 var bodyOne = document.querySelector("body");
 bodyOne.style.background = color;
+
+function randomBackground() {
+	//pick a red from 0-255
+	var r = Math.floor(Math.random() * 256)
+	//pick a green from 0-255
+	var g = Math.floor(Math.random() * 256)
+	//pick a blue from 0-255
+	var b = Math.floor(Math.random() * 256)
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
